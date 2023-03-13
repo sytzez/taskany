d98d620bfc3f2195e79e997dc8bb8b2ed9a2b96b
sytzez <sytzez@gmail.com>
2023-03-13 00:07:39 +0000

    Add projects to tasks routing spec

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe TasksController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/tasks").to route_to("tasks#index")
+      expect(get: "/projects/1/tasks").to route_to("tasks#index", project_id: "1")
     end
 
     it "routes to #new" do
-      expect(get: "/tasks/new").to route_to("tasks#new")
+      expect(get: "/projects/1/tasks/new").to route_to("tasks#new", project_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/tasks/1").to route_to("tasks#show", id: "1")
+      expect(get: "/projects/1/tasks/2").to route_to("tasks#show", id: "2", project_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/tasks/1/edit").to route_to("tasks#edit", id: "1")
+      expect(get: "/projects/1/tasks/2/edit").to route_to("tasks#edit", id: "2", project_id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/tasks").to route_to("tasks#create")
+      expect(post: "/projects/1/tasks").to route_to("tasks#create", project_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/tasks/1").to route_to("tasks#update", id: "1")
+      expect(put: "/projects/1/tasks/2").to route_to("tasks#update", id: "2", project_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/tasks/1").to route_to("tasks#update", id: "1")
+      expect(patch: "/projects/1/tasks/2").to route_to("tasks#update", id: "2", project_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/tasks/1").to route_to("tasks#destroy", id: "1")
+      expect(delete: "/projects/1/tasks/2").to route_to("tasks#destroy", id: "2", project_id: "1")
     end
   end
 end
