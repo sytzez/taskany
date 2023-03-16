01f9983046e846998f6ac990c8acf233d2a795cd
sytzez <sytzez@gmail.com>
2023-03-16 22:59:34 +0000

    Update tasks index view spec to use correct header level

--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "tasks/index", type: :view do
 
   it "renders a list of tasks" do
     render
-    assert_select 'h2', text: Regexp.new(tasks[0].title)
-    assert_select 'h2', text: Regexp.new(tasks[1].title)
+    assert_select 'h3', text: Regexp.new(tasks[0].title)
+    assert_select 'h3', text: Regexp.new(tasks[1].title)
   end
 end
