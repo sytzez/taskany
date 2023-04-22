36d28eff6e2e28d947a7f1142fc0fc51f442e8c2
sytzez <sytzez@gmail.com>
2023-04-22 18:21:51 +0100

    Restyle board columns

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.slim',
+    './app/components/*.slim',
   ],
   theme: {
     extend: {
